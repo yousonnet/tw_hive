0d3b9025452c4a8583b478cd5457fd8b71f88a36
yousonnet <orionxiaohan@gmail.com>
2024-10-21 09:22:17 +0800

    ft

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import uvicorn
+from dotenv import load_dotenv
+import os
+
+port = os.getenv('host_port')
 
 if __name__ == "__main__":
-    uvicorn.run("a_hive_router:app", host="0.0.0.0", port=7666, reload=False)
+    uvicorn.run("a_hive_router:app", host="0.0.0.0", port=int(port), reload=False)
